0176fbb0e1d99859acc2f84aa338075dee1879d3
Simon Bäumer <bs@shopware.com>
2016-07-11 13:58:43 +0200

    PT-6192 - Implement update method

--- SwagBackendOrder.php
+++ SwagBackendOrder.php
@@ -9,10 +9,13 @@
 namespace SwagBackendOrder;
 
 use Shopware\Components\Plugin;
+use Shopware\Components\Plugin\Context\UpdateContext;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class SwagBackendOrder extends Plugin
 {
+    const MIN_VERSION = '5.2.0';
+
     /**
      * @param ContainerBuilder $container
      */
@@ -22,30 +25,37 @@ class SwagBackendOrder extends Plugin
 
         parent::build($container);
     }
-//
-//    /**
-//     * @param string $oldVersion
-//     * @return bool
-//     * @throws Exception
-//     */
-//    public function update($oldVersion)
-//    {
-//        if (!$this->assertMinimumVersion($this->getMinimumVersion())) {
-//            throw new Exception(
-//                sprintf('This plugin requires Shopware %s or a later version', $this->getMinimumVersion())
-//            );
-//        }
-//
-//        if (version_compare($oldVersion, '1.0.1', '<')) {
-//            $orderDetailIds = Shopware()->Db()->fetchCol(
-//                'SELECT id FROM s_order_details WHERE id NOT IN (SELECT detailID FROM s_order_details_attributes);'
-//            );
-//            foreach ($orderDetailIds as $orderDetailId) {
-//                $sql = 'INSERT INTO `s_order_details_attributes` (detailID) VALUES (?)';
-//                Shopware()->Db()->query($sql, [$orderDetailId]);
-//            }
-//        }
-//
-//        return true;
-//    }
+
+    /**
+     * @param UpdateContext $context
+     * @throws \Exception
+     */
+    public function update(UpdateContext $context)
+    {
+        if (!$context->assertMinimumVersion(self::MIN_VERSION)) {
+            throw new \Exception(
+                sprintf('This plugin requires Shopware %s or a later version', self::MIN_VERSION)
+            );
+        }
+
+        if (version_compare($context->getUpdateVersion(), '1.0.1', '>=')) {
+            $this->updateTo101();
+        }
+
+        return parent::update($context);
+    }
+
+    /**
+     * Update Backend Orders to version 1.0.1
+     */
+    private function updateTo101()
+    {
+        $orderDetailIds = $this->container->get('db')->fetchCol(
+            'SELECT id FROM s_order_details WHERE id NOT IN (SELECT detailID FROM s_order_details_attributes);'
+        );
+        foreach ($orderDetailIds as $orderDetailId) {
+            $sql = 'INSERT INTO `s_order_details_attributes` (detailID) VALUES (?)';
+            $this->container->get('db')->query($sql, [$orderDetailId]);
+        }
+    }
 }
