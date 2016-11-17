0be578151ec455018edb45d766fef21f4a5829d0
Kevinschmid86 <Kevin_Jaeck@web.de>
2016-11-17 15:20:45 +0100

    Tried to fix codestyling for shopware conventions https://github.com/shopware/shopware/blob/5.2/CONTRIBUTING.md#coding-standards

--- Components/ProductRepository.php
+++ Components/ProductRepository.php
@@ -29,7 +29,6 @@ class ProductRepository
     /**
      * @param string $search
      * @param string $groupKey
-     *
      * @return \Doctrine\ORM\QueryBuilder|\Shopware\Components\Model\QueryBuilder
      */
     public function getProductQueryBuilder($search, $groupKey = 'EK')
