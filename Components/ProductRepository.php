0c8b371f3025332b758d41a230fa368b81fa36e6
Kevinschmid86 <Kevin_Jaeck@web.de>
2017-08-09 19:19:31 +0200

    Commit for Merge Conflicts

--- Components/ProductRepository.php
+++ Components/ProductRepository.php
@@ -8,8 +8,9 @@
 
 namespace SwagBackendOrder\Components;
 
-use Doctrine\ORM\Query\Expr;
+use Doctrine\ORM\Query\Expr\Join;
 use Shopware\Components\Model\ModelManager;
+use Shopware\Components\Model\QueryBuilder;
 use Shopware\Models\Article\Article;
 use Shopware\Models\Article\Supplier;
 
@@ -30,20 +31,22 @@ class ProductRepository
 
     /**
      * @param string $search
+     * @param string $customerGroupKey
      *
-     * @return \Doctrine\ORM\QueryBuilder|\Shopware\Components\Model\QueryBuilder
+     * @return QueryBuilder
      */
-    public function getProductQueryBuilder($search)
+    public function getProductQueryBuilder($search, $customerGroupKey = 'EK')
     {
         $builder = $this->modelManager->createQueryBuilder();
 
         /*
-         * query to search for article variants or the article ordernumber
-         * the query concats the article name and the additional text field for the search
+         * query to search for product variants or the product number
+         * the query concatenate the product name and the additional text field for the search
          */
         $builder->select(
             'articles.id AS articleId,
             details.number,
+            details.id AS variantId,
             articles.name,
             details.id,
             details.inStock,
@@ -56,34 +59,36 @@ class ProductRepository
         );
 
         $builder->from(Article::class, 'articles')
-            ->leftJoin('articles.details', 'details')
-            ->leftJoin('details.prices', 'prices')
-            ->leftJoin('articles.tax', 'tax')
-            ->leftJoin(
-                Supplier::class,
-                'sp',
-                Expr\Join::WITH,
-                'articles.supplierId = sp.id'
-            )
-            ->where(
-                $builder->expr()->like(
-                    $builder->expr()->concat(
-                        'articles.name',
+                ->leftJoin('articles.details', 'details')
+                ->leftJoin('articles.supplier', 'sp')
+                ->leftJoin('details.prices', 'prices', Join::WITH, 'prices.customerGroupKey = :groupKey');
+
+        if ($customerGroupKey !== 'EK') {
+            $builder->leftJoin('details.prices', 'fallbackPrices', Join::WITH, "fallbackPrices.customerGroupKey = 'EK'");
+            $builder->addSelect('fallbackPrices.price AS fallbackPrice');
+        }
+
+        $builder->leftJoin('articles.tax', 'tax')
+                ->where(
+                    $builder->expr()->like(
                         $builder->expr()->concat(
-                            $builder->expr()->literal(' '),
-                            'details.additionalText'
-                        )
-                    ),
-                    $builder->expr()->literal($search)
+                            'articles.name',
+                            $builder->expr()->concat(
+                                $builder->expr()->literal(' '),
+                                'IFNULL(details.additionalText,\'\')'
+                            )
+                        ),
+                        $builder->expr()->literal($search)
+                    )
                 )
-            )
-            ->orWhere('details.number LIKE :number')
-            ->orWhere('sp.name LIKE :number')
-            ->andWhere('articles.active = 1')
-            ->setParameter('number', $search)
-            ->orderBy('details.number')
-            ->groupBy('details.number')
-            ->setMaxResults(8);
+                ->orWhere('details.number LIKE :number')
+                ->orWhere('sp.name LIKE :number')
+                ->andWhere('articles.active = 1')
+                ->setParameter('number', $search)
+                ->setParameter('groupKey', $customerGroupKey)
+                ->orderBy('details.number')
+                ->groupBy('details.number')
+                ->setMaxResults(3);
 
         return $builder;
     }
