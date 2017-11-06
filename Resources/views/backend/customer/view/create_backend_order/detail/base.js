bc3c9a854a00f22f062a8aa93fc803337a1c39a9
Mrtn Schndlr <mrtnschndlr@gmail.com>
2017-11-06 13:16:25 +0100

    PT-8615 - Fix email validation Fixes https://github.com/shopwareLabs/SwagBackendOrder/pull/14/

--- Resources/views/backend/customer/view/create_backend_order/detail/base.js
+++ Resources/views/backend/customer/view/create_backend_order/detail/base.js
@@ -35,12 +35,12 @@ Ext.define('Shopware.apps.CreateBackendOrder.view.Base', {
                 listeners: {
                     scope: me,
                     afterrender: function (field) {
-                        // only validates the email field if the mail is not the guest account email which can be configured in the plugin config
-                        if (field.getValue() != me.record.get('email')) {
-                            window.setTimeout(function () {
+                        window.setTimeout(function () {
+                            // only validates the email field if the mail is not the guest account email which can be configured in the plugin config
+                            if (field.getValue() != me.record.get('email')) {
                                 field.validationUrl = '{url action="validateEmail"}';
-                            }, 500);
-                        }
+                            }
+                        }, 500);
                     }
                 }
             });
